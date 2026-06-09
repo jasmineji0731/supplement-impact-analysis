d85df3bd325f2cbbca83a84f75f8f04e69acb3cc
Jasmine Ji <jasmineji20060731@gmail.com>
2026-06-09 22:27:58 +0800

    Update 2_Data_Visualization.py

--- pages/pages/2_Data_Visualization.py
+++ pages/pages/2_Data_Visualization.py
@@ -6,3 +6,49 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("cleaned_supplement_data.csv")
 
 st.title("Data Visualization")
+st.header("Initial Weight vs Final Weight")
+
+fig, ax = plt.subplots()
+
+sns.scatterplot(
+    data=df,
+    x="Initial_WT",
+    y="Final_WT",
+    ax=ax
+)
+
+st.pyplot(fig)
+st.header("Final Weight by Supplement")
+
+fig, ax = plt.subplots()
+
+sns.boxplot(
+    data=df,
+    x="Supplement",
+    y="Final_WT",
+    ax=ax
+)
+
+st.pyplot(fig)
+st.header("Final Weight by Supplement")
+
+fig, ax = plt.subplots()
+
+sns.boxplot(
+    data=df,
+    x="Supplement",
+    y="Final_WT",
+    ax=ax
+)
+
+st.pyplot(fig)
+st.header("Business Insights")
+
+st.write("""
+1. Combined supplement use produced the highest average final weight.
+
+2. Creatine generated higher strength gains than Mass Gainer.
+
+3. The dataset is balanced across gender groups.
+""")
+
